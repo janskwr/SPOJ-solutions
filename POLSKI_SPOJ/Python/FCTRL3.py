49c164976fd9306e1ee80ae28253ffada6fdbd57
Janek <62259501+ManWithNoName-code@users.noreply.github.com>
2020-05-19 17:39:06 +0200

    Create FCTRL3.py

---
+++ POLSKI_SPOJ/Python/FCTRL3.py
@@ -0,0 +1,11 @@
+number_of_cases = input()
+for x in range(int(number_of_cases)):
+    num = int(input())
+    if num == 0 or num == 1: print("0 1")
+    elif num == 2: print("0 2")
+    elif num == 3: print("0 6")
+    elif num == 4: print("2 4")
+    elif num == 5 or num == 6 or num == 8: print("2 0")
+    elif num == 7: print("4 0")
+    elif num == 9: print("8 0")
+    else: print("0 0")
